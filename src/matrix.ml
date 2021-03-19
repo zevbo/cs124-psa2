48953266f12cd811a7325554f3f358c505243208
zevbo <minsky-pz@bxscience.edu>
2021-03-19 17:43:11 -0400

    changing to List.sum

--- src/matrix.ml
+++ src/matrix.ml
@@ -9,6 +9,7 @@ let set t row col v =
   Array.set t.m.(col) row v
 let get t row col = 
   t.m.(col).(row)
+(* like create, except rather than a singular value, it takes a function of form row -> col -> val *)
 let make ~h ~w ~f = 
   let final_matrix = create ~h ~w ~default:(f 0 0) in 
   let calc_and_set_cell (row, col) = 
@@ -30,9 +31,8 @@ let add t1 t2 =
   make ~h:t1.h ~w:t1.w ~f:(fun row col -> (get t1 row col) + (get t2 row col))
 let mult_normal t1 t2 = 
   check_mult_compatible t1 t2;
-  let size = t1.w in
   let calculate_cell row col = 
-    List.fold_left (List.range 0 size) ~init:0 ~f:(fun sum i -> sum + (get t1 row i) * (get t2 col i))
+    List.sum (module Int) (List.range 0 t1.w) ~f:(fun i -> (get t1 row i) * (get t2 i col))
   in
   make ~h:t1.h ~w:t2.w ~f:calculate_cell
 let mult_stras t1 t2 _min_size = 
