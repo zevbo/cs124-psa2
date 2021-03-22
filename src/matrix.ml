a42d08045b3cb04eee11c34b72e45b8c8552d1fa
zevbo <minsky-pz@bxscience.edu>
2021-03-22 18:24:03 -0400

    it builds now :)

--- src/matrix.ml
+++ src/matrix.ml
@@ -14,9 +14,7 @@ let init ~h ~w ~f =
   let init_column col = 
     Array.init h ~f:(fun row -> f row col) 
   in
-  Array.init w ~f:init_column
-  
-exception Unimplmented of string
+  {h; w; m = Array.init w ~f:init_column}
 
 let check_add_compatible t1 t2 = 
   assert (t1.w = t2.w && t1.h = t2.h)
@@ -29,7 +27,7 @@ let add t1 t2 =
 let subtract t1 t2 = 
   check_add_compatible t1 t2;
   init ~h:t1.h ~w:t1.w ~f:(fun row col -> (get t1 row col) - (get t2 row col))
-  let mult_normal t1 t2 = 
+let mult_normal t1 t2 = 
   check_mult_compatible t1 t2;
   let calculate_cell row col = 
     List.sum (module Int) (List.range 0 t1.w) ~f:(fun i -> (get t1 row i) * (get t2 i col))
@@ -67,27 +65,28 @@ let make_from_submatrices t11 t12 t21 t22 =
   
 let rec mult_stras t1 t2 _min_size = 
   check_mult_compatible t1 t2;
+  let mult_stras t1 t2 = mult_stras t1 t2 _min_size in
   (*Assumes that matrices are square and have sizes that are powers of 2*)
   if t1.h <= _min_size then mult_normal t1 t2 else (
-    let A = submatrix t1 0 t1.h/2 0 t1.w/2 in
-    B = submatrix t1 0 t1.h/2 t1.w/2 t1.w
-    C = submatrix t1 t1.h/2 t1.h 0 t1.w/2
-    D = submatrix t1 t1.h/2 t1.h t1.w/2 t1.w
-    E = submatrix t2 0 t2.h/2 0 t2.w/2
-    F = submatrix t2 0 t2.h/2 t2.w/2 t2.w
-    G = submatrix t2 t2.h/2 t2.h 0 t2.w/2
-    H = submatrix t2 t2.h/2 t2.h t2.w/2 t2.w
+    let a = submatrix t1 0 (t1.h/2) 0 (t1.w/2) in
+    let b = submatrix t1 0 (t1.h/2) (t1.w/2) t1.w in
+    let c = submatrix t1 (t1.h/2) t1.h 0 (t1.w/2) in
+    let d = submatrix t1 (t1.h/2) t1.h (t1.w/2) t1.w in
+    let e = submatrix t2 0 (t2.h/2) 0 (t2.w/2) in
+    let f = submatrix t2 0 (t2.h/2) (t2.w/2) t2.w in
+    let g = submatrix t2 (t2.h/2) t2.h 0 (t2.w/2) in
+    let h = submatrix t2 (t2.h/2) t2.h (t2.w/2) t2.w in
 
-    P1 = mult_stras A (subtract F H) _min_size
-    P2 = mult_stras (add A B) H _min_size
-    P3 = mult_stras (add C D) E _min_size
-    P4 = mult_stras D (subtract G E) _min_size
-    P5 = mult_stras (add A D) (add E H) _min_size
-    P6 = mult_stras (subtract B D) (add G H) _min_size
-    P7 = mult_stras (subtract A C) (add E F)
-    t11 = add (add P5 P4) (subtract P6 P2)
-    t12 = add P1 P2
-    t21 = add P3 P4
-    t22 = subtract (add P5 P1) (add P3 P7)
+    let p1 = mult_stras a (subtract f h) in
+    let p2 = mult_stras (add a b) h in
+    let p3 = mult_stras (add c d) e in
+    let p4 = mult_stras d (subtract g e) in
+    let p5 = mult_stras (add a d) (add e h) in
+    let p6 = mult_stras (subtract b d) (add g h) in
+    let p7 = mult_stras (subtract a c) (add e f) in
+    let t11 = add (add p5 p4) (subtract p6 p2) in
+    let t12 = add p1 p2 in
+    let t21 = add p3 p4 in
+    let t22 = subtract (add p5 p1) (add p3 p7) in
     make_from_submatrices t11 t12 t21 t22
   )
--- src/matrix.mli
+++ src/matrix.mli
@@ -1,5 +1,7 @@
 type 'a t = {h:int; w:int; m: 'a array array}
 
 val create : h:int -> w:int -> default:'a -> 'a t
-val mult_normal : 'a t -> 'a t -> int -> 'a t
-val mult_stras : 'a t -> 'a t -> int -> 'a t
+val mult_normal : int t -> int t -> int t
+val mult_stras : int t -> int t -> int -> int t
+
+val set : 'a t -> int -> int -> 'a -> unit
